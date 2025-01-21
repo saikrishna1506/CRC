98541d646ba94cb2a760f8e96b0f0db3f6807039
saikrishna1506 <108617880+saikrishna1506@users.noreply.github.com>
2025-01-21 13:13:29 +0530

    dbline

--- backend/chatbot.js
+++ backend/chatbot.js
@@ -88,7 +88,7 @@ app.use(cors()); // Add this line to enable CORS
 app.use(bodyParser.json());
 app.get('/',(req,res)=>{
   console.log("hello server");
-  return res.json({ response: 'Hello World!' });
+   res.json("Hello World!");
 })
 // API to handle chatbot interaction
 app.post('/chat', async (req, res) => {
